e1066f21bb973053a8f33b7ed8a057cb6c087bdf
Kirill Garbar <kirill@iterium.co.uk>
2021-04-01 14:21:20 +0100

    Upgrading sentry, removing env lib from requirements

--- go.mod
+++ go.mod
@@ -3,9 +3,6 @@ module github.com/facktoreal/sentry
 go 1.12
 
 require (
-	github.com/certifi/gocertifi v0.0.0-20190506164543-d2eda7129713 // indirect
-	github.com/facktoreal/env v0.0.0-20200608210745-c5802646b123
-	github.com/getsentry/raven-go v0.2.0
-	github.com/labstack/echo/v4 v4.1.6
-	github.com/pkg/errors v0.8.1 // indirect
+	github.com/getsentry/sentry-go v0.10.0
+	github.com/labstack/echo/v4 v4.1.11
 )
--- main.go
+++ main.go
@@ -2,39 +2,65 @@ package sentry
 
 import (
 	"errors"
+	"log"
+	"os"
 
-	"github.com/facktoreal/env"
-	"github.com/getsentry/raven-go"
+	sentryGo "github.com/getsentry/sentry-go"
 )
 
 // Init sentry configuration
 func Init() error {
-	if !env.MustPresent("SENTRY_DSN") {
+	if !MustPresent("SENTRY_DSN") {
 		return errors.New("'SENTRY_DSN' must be set")
 	}
 
-	if !env.MustPresent("ENV") {
+	if !MustPresent("ENV") {
 		return errors.New("'ENV' must be set")
 	}
 
 	// Set release version
-	if !env.MustPresent("RELEASE") {
+	if !MustPresent("RELEASE") {
 		return errors.New("'RELEASE' must be set")
 	}
 
-	// Enable sentry integration
-	if err := raven.SetDSN(env.MustGetString("SENTRY_DSN")); err != nil {
+	err := sentryGo.Init(sentryGo.ClientOptions{
+		Dsn: MustGetString("SENTRY_DSN"),
+		Environment: MustGetString("ENV"),
+		Release: MustGetString("RELEASE"),
+	})
+
+	if err != nil {
 		return err
 	}
-
-	// Configure sentry
-	raven.SetRelease(env.MustGetString("RELEASE"))
-	raven.SetEnvironment(env.MustGetString("ENV"))
 
 	return nil
 }
 
-// CaptureErrorAndWait ...
-func CaptureErrorAndWait(err error, tags map[string]string, interfaces ...raven.Interface) string {
-	return raven.CaptureErrorAndWait(err, tags, interfaces...)
+// CaptureError ...
+func CaptureError(err error, tags map[string]string) {
+	sentryGo.WithScope(func(scope *sentryGo.Scope) {
+		scope.SetContext("Request", tags)
+
+		sentryGo.CaptureException(err)
+	})
+}
+
+// MustPresent ...
+func MustPresent(key string) bool {
+	v := os.Getenv(key)
+	if v == "" {
+		return false
+	}
+
+	return true
+}
+
+// MustGetString ...
+func MustGetString(key string) string {
+	v := os.Getenv(key)
+	if v == "" {
+		log.Panicf("%s environment variable not set.", key)
+	}
+
+	return v
 }
